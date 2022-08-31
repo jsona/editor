eaa44b92b3905856c79a4ef5082618c490e31ea3
sigoden <sigoden@gmail.com>
2022-08-31 11:21:14 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jsona editor
---
+++ packages/apps/src/editorWorker.ts
@@ -0,0 +1,15 @@
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+
+// @ts-ignore
+self.MonacoEnvironment = {
+	getWorker(_: any, label: string) {
+		if (label === 'json') {
+			return new jsonWorker();
+		}
+		return new editorWorker();
+	}
+};
+
+monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
