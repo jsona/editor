26026a97c5f8928ce2c4dbf3fafe0a18fb9dd576
sigoden <sigoden@gmail.com>
2022-09-01 16:09:45 +0800

    feat: lsp accept options

--- packages/apps/src/main.tsx
+++ packages/apps/src/main.tsx
@@ -1,19 +1,18 @@
 import 'monaco-editor/esm/vs/editor/editor.all.js';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import jsonaWorker from "monaco-jsona/dist/jsona.worker.js?worker";
-import { register, startLsp, DEFAULT_CONFIGURATION } from "monaco-jsona";
+import { register, startLsp } from "monaco-jsona";
 
 let uri = monaco.Uri.parse('root:///input.jsona');
 let worker = new jsonaWorker();
-let c = startLsp(worker);
-c.sendNotification("internal/setup", { debug: import.meta.env.DEV });
-c.onRequest("workspace/configuration", async (parmas) => {
-  return Array.from(Array(parmas.length)).map(() => DEFAULT_CONFIGURATION);
-});
-c.onNotification("jsona/initializeWorkspace", () => {
-  c.sendRequest("jsona/associatedSchema", { documentUri: uri.toString() });
-});
 register(monaco);
+startLsp({
+  worker,
+  debug: import.meta.env.DEV,
+  getDocUris() {
+    return [uri.toString()];
+  }
+});
 monaco.editor.create(document.getElementById("container"), {
   model: monaco.editor.getModel(uri) || monaco.editor.createModel(`{ @jsonaschema("schema") }`, 'jsona', uri),
   glyphMargin: true,
--- packages/monaco-jsona/src/index.ts
+++ packages/monaco-jsona/src/index.ts
@@ -7,69 +7,80 @@ import getNotificationServiceOverride from 'vscode/service-override/notification
 import getDialogsServiceOverride from 'vscode/service-override/dialogs'
 export { MonacoLanguageClient };
 
-export const DEFAULT_CONFIGURATION = {
-  "executable": {
-    "bundled": false,
-    "path": null,
-    "environment": {
-    },
-    "extraArgs": null
-  },
-  "schema": {
-    "enabled": true,
-    "associations": {
-    },
-    "storeUrl": "https://cdn.jsdelivr.net/npm/@jsona/schemastore@latest/index.json",
-    "cache": false
-  },
-  "formatter": {
-    "indentString": "  ",
-    "trailingNewline": false,
-    "trailingComma": false,
-    "formatKey": false
-  }
-}
-
 export function register(monaco: typeof Monaco) {
   monaco.languages.register(extensionPoint);
   monaco.languages.setMonarchTokensProvider(extensionPoint.id, monarchLanguage);
   monaco.languages.setLanguageConfiguration(extensionPoint.id, languageConfiguration);
 }
 
-export function startLsp(worker: Worker) {
+export interface StartLspOptions {
+  worker: Worker,
+  debug: boolean,
+  getDocUris: () => string[],
+}
+
+export function startLsp(options: StartLspOptions) {
   console.log("start jsona lsp");
   StandaloneServices.initialize({
     ...getNotificationServiceOverride(),
     ...getDialogsServiceOverride(),
   });
   MonacoServices.install();
-  const reader = new BrowserMessageReader(worker);
-  const writer = new BrowserMessageWriter(worker);
+  const reader = new BrowserMessageReader(options.worker);
+  const writer = new BrowserMessageWriter(options.worker);
   const languageClient = createLanguageClient({ reader, writer });
   languageClient.start();
-
+  languageClient.sendNotification("internal/setup", { debug: options.debug });
+  languageClient.onNotification("jsona/initializeWorkspace", async () => {
+    for (const documentUri of options.getDocUris()) {
+      await languageClient.sendRequest("jsona/associatedSchema", { documentUri });
+    }
+  });
+  languageClient.onRequest("workspace/configuration", async (parmas) => {
+    return Array.from(Array(parmas.length)).map(() => ({
+      "executable": {
+        "bundled": false,
+        "path": null,
+        "environment": {},
+        "extraArgs": null
+      },
+      "schema": {
+        "enabled": true,
+        "associations": {
+        },
+        "storeUrl": "https://cdn.jsdelivr.net/npm/@jsona/schemastore@latest/index.json",
+        "cache": false
+      },
+      "formatter": {
+        "indentString": "  ",
+        "trailingNewline": false,
+        "trailingComma": false,
+        "formatKey": false
+      }
+    }));
+  });
   reader.onClose(() => languageClient.stop());
 
   return languageClient;
 }
 
-function createLanguageClient (transports: MessageTransports): MonacoLanguageClient {
-    return new MonacoLanguageClient({
-        name: 'JSONA Language Server',
-        clientOptions: {
-            // use a language id as a document selector
-            documentSelector: [{ language: 'jsona' }],
-            // disable the default error handler
-            errorHandler: {
-                error: () => ({ action: ErrorAction.Continue }),
-                closed: () => ({ action: CloseAction.DoNotRestart })
-            }
-        },
-        // create a language client connection to the server running in the web worker
-        connectionProvider: {
-            get: () => {
-                return Promise.resolve(transports);
-            }
-        }
-    });
+function createLanguageClient(transports: MessageTransports): MonacoLanguageClient {
+  return new MonacoLanguageClient({
+    name: 'JSONA Language Server',
+    clientOptions: {
+      // use a language id as a document selector
+      documentSelector: [{ language: 'jsona' }],
+      // disable the default error handler
+      errorHandler: {
+        error: () => ({ action: ErrorAction.Continue }),
+        closed: () => ({ action: CloseAction.DoNotRestart })
+      }
+    },
+    // create a language client connection to the server running in the web worker
+    connectionProvider: {
+      get: () => {
+        return Promise.resolve(transports);
+      }
+    }
+  });
 }
